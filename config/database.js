2aa524ce149549d8c87381f78f25b0721ae7be34
Xinyu Tu <carson_tu@hotmail.com>
2024-08-16 21:44:46 -0400

    udpate database.js with log

--- config/database.js
+++ config/database.js
@@ -23,6 +23,13 @@ const connectDB = async () => {
     }
   }  */
 
+  console.log('Primary MongoDB URI:', primaryUri);  //打印 Github Page 变量排错
+
+  if (!primaryUri || !primaryUri.startsWith('mongodb')) {
+    console.error('Invalid or missing MongoDB URI');
+    throw new Error('Failed to connect to primary MongoDB');
+  }
+
   try {
     await mongoose.connect(primaryUri);
     console.log('Connected to primary MongoDB');
