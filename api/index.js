875e0c8419e8fa8f66ae27517765954ccf1b7e26
Xinyu Tu <carson_tu@hotmail.com>
2024-08-20 02:43:31 -0400

    reverse

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import auth from '../routes/auth.js';
+import authRoutes from '../routes/auth.js';
 import forumRoutes from '../routes/forumRoutes.js';
 import commentRoutes from '../routes/commentRoutes.js';
 import { connectDB } from '../config/database.js';
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 路由设置
-app.use('/auth', auth);
+app.use('/auth', authRoutes);
 app.use('/forum', forumRoutes);
 app.use('/comments', commentRoutes);
 
