d97053f80f05bf121d7640a71f4953505ce69276
Xinyu Tu <carson_tu@hotmail.com>
2024-08-23 22:55:12 -0400

    ss

--- routes/forumRoutes.js
+++ routes/forumRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { getPosts, createPost, getPost } from '../controllers/forumController.js';
-import upload, { handleUpload } from '../middleware/upload.js';
 
 const router = express.Router();
 
 router.get('/posts', getPosts);
-router.post('/post', upload.single('file'), handleUpload, createPost);
+router.post('/post', createPost);
 router.get('/post/:id', getPost);
 
 export default router;
